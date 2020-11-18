d8b7e5adb58c5a397274a4830d8b490edd4d4696
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-18 07:35:50 +0530

    Create contact: Define various Person detail

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -2,5 +2,6 @@
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address book Program !");
+        ContactPerson person = new ContactPerson();
     }
 }
